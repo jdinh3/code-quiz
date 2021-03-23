9a04893a33daba419e2940b28c729e68cb8b4a08
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-03-22 23:15:09 -0500

    added buttons, styling, and event listeners in js

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+// Variables
+var startButton = document.getElementById("start-btn");
+var questionContainerEl = document.getElementById("quiz-container");
+var nextButton = document.getElementById("next-btn");
+var introEl = document.getElementById("intro-page");
+
+// Quiz Questions
+
+// Event listener for start button
+
+startButton.addEventListener("click", startQuiz);
+
+// Functions
+function startQuiz() {
+  console.log("Quiz has started");
+  startButton.style.display = "none";
+  introEl.style.display = "none";
+  nextButton.classList.remove("hide");
+  questionContainerEl.classList.remove("hide");
+}
+
+function nextQuestion() {}
+
+function selectAnswer() {}
+
+// Function Calls
